94b08e4fc157b0dcd7ee33372beaa37afa6ac398
BrunoGomes14 <brunogda729@gmail.com>
2020-04-16 23:12:56 -0300

    Criando estrutura

---
+++ main.c
@@ -0,0 +1,19 @@
+#include <stdlib.h>
+#include <stdio.h>
+
+int main()
+{
+    char cNome[55];
+
+    printf("Bem-vindo a UniOndonto, Qual seu nome? \n");
+    scanf("%s", &cNome);
+    
+    printf("Ola %s \n", cNome);
+
+    printf("Selecione como deseja continuar");
+    
+
+    system("PAUSE");
+    return 0;
+}
+    
